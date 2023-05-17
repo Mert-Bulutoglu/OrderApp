32244c660dd70248be0968c8931be7d3a3f6b175
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-05-17 14:25:15 +0300

    add entities

---
+++ OrderApp.Domain/Concrete/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using OrderApp.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Domain.Concrete.Entities
+{
+    public class Order : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerEmail { get; set; }
+        public string? CustomerGSM { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
