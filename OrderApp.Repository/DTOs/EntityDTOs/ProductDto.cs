30ea76aa29280f5f1173ee5476af6ad1033b9089
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-05-17 14:43:54 +0300

    complete core layer basics

---
+++ OrderApp.Repository/DTOs/EntityDTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Repository.DTOs.EntityDtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public string? Unit { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ OrderApp.Repository/Services/IGenericService.cs
@@ -0,0 +1,23 @@
+﻿using OrderApp.Repository.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Repository.Services
+{
+    public interface IGenericService<T, TDto> where T : class
+    {
+        public Task<ApiResponseDto<TDto>> GetByIdAsync(int id);
+        public Task<ApiResponseDto<TDto>> GetAllAsync();
+        public Task<ApiResponseDto<List<TDto>>> Where(Expression<Func<T, bool>> expression);
+        public Task<ApiResponseDto<TDto>> AnyAsync(Expression<Func<T, bool>> expression);
+        public Task<ApiResponseDto<TDto>> AddAsync(TDto entity);
+        public Task<ApiResponseDto<List<TDto>>> AddRangeAsync(List<TDto> entities);
+        public Task<ApiResponseDto<TDto>> UpdateAsync(TDto entity);
+        public Task<ApiResponseDto<TDto>> RemoveAsync(int id);
+        public Task<ApiResponseDto<List<TDto>>> RemoveRangeAsync(List<TDto> entities);
+    }
+}
