44d5daac696f18027878062d48246b56d4fa8599
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-05-20 12:25:49 +0300

    complete rabbitMQ

---
+++ OrderApp.RabbitMQ/Services/MailSenderBackgroundService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using OrderApp.Repository.SMTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace OrderApp.RabbitMQ.Services
+{
+    public class MailSenderBackgroundService : IMailSenderBackgroundService
+    {
+        private readonly IConfiguration _config;
+        private readonly IConnection _rabbitMQConnection;
+
+        public MailSenderBackgroundService(IConfiguration config)
+        {
+            _config = config;
+            _rabbitMQConnection = CreateRabbitMQConnection();
+        }
+        public async Task SendMailAsync(string to, string subject, string body, bool isBodyHtml = true)
+        {
+            await SendMailAsync(new[] { to }, subject, body, isBodyHtml);
+        }
+
+        public async Task SendMailAsync(string[] tos, string subject, string body, bool isBodyHtml = true)
+        {
+
+            using (var channel = _rabbitMQConnection.CreateModel())
+            {
+                string queueName = _config["RabbitMQConfiguration:QueueName"];
+                channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                foreach (var to in tos)
+                {
+                    var routingKey = $"OrderSuccessfullyCompleted.{to}";
+                    var messageBody = Encoding.UTF8.GetBytes("Order successfully completed.");
+
+                    channel.BasicPublish("", queueName, properties, messageBody);
+                }
+            }
+
+            MailMessage mail = new();
+            mail.IsBodyHtml = isBodyHtml;
+            foreach (var to in tos)
+                mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.From = new(_config["Mail:Username"], _config["Mail:Webname"], System.Text.Encoding.UTF8);
+
+            SmtpClient smtp = new();
+            smtp.Credentials = new NetworkCredential(_config["Mail:Username"], _config["Mail:Password"]);
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Host = _config["Mail:Host"];
+            await smtp.SendMailAsync(mail);
+        }
+
+
+        public async Task SendSuccessMailAsync(string to)
+        {
+            StringBuilder mail = new();
+            mail.AppendLine("Hello<br>Order successfully completed.</br><br>");
+            await SendMailAsync(to, "Nice!", mail.ToString());
+        }
+
+        private IConnection CreateRabbitMQConnection()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _config["RabbitMQConfiguration:HostName"],
+                UserName = _config["RabbitMQConfiguration:UserName"],
+                Password = _config["RabbitMQConfiguration:Password"]
+            };
+
+            return factory.CreateConnection();
+        }
+    }
+}
