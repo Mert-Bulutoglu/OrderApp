e22d9cb957538539a5bbadaf7ad545d00427ec77
Mert Bulutoglu <mertbulutoglu95@gmail.com>
2023-05-20 13:53:50 +0300

    complete all cases

--- OrderApp/Controllers/OrderController.cs
+++ OrderApp/Controllers/OrderController.cs
@@ -14,27 +14,8 @@ namespace OrderApp.API.Controllers
             _service = orderService;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> All(List<FilterDTO> Filters)
-        {
-            return Ok(await _service.GetAllAsync(Filters));
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            return Ok(await _service.GetByIdAsync(id));
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Remove(int id)
-        {
-
-            return Ok(await _service.RemoveAsync(id));
-        }
-
         [HttpPost("[action]")]
-        public async Task<IActionResult> Save(CreateOrderRequestDto createOrderRequestDto)
+        public async Task<IActionResult> CreateOrder(CreateOrderRequestDto createOrderRequestDto)
         {
             return Ok(await _service.AddAsync(createOrderRequestDto));
         }
